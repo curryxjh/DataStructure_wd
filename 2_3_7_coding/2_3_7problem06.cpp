ad0b6042daaef7085383522c50250332dd7efd15
curryxjh <2648108535@qq.com>
2023-07-30 23:05:37 +0800

    finish 2_3_7 problem06

--- 2_3_7_coding/2_3_7problem06.cpp
+++ 2_3_7_coding/2_3_7problem06.cpp
@@ -19,20 +19,39 @@ bool Init(HeaderList* L); // 初始化单链表
 bool Insert(HeaderList* L, int i, int x); // 在ai之后插入一个新元素
 bool Print(HeaderList* L); // 输出
 bool Destory(HeaderList* L); // 销毁单链表
+bool Sort_list(HeaderList* L);
 
 int main()
 {
-//    HeaderList* headerList = new HeaderList;
-//    Init(headerList);
-//    Insert(headerList, -1, 2);
-//    Insert(headerList, -1, 27);
-//    Insert(headerList, -1, 1);
-//    Insert(headerList, -1, 3);
-//    Insert(headerList, -1, 4);
-//    Insert(headerList, -1, 5);
-//    Print(headerList);
-
+    HeaderList* headerList = new HeaderList;
+    Init(headerList);
+    Insert(headerList, -1, 2);
+    Insert(headerList, -1, 27);
+    Insert(headerList, -1, 1);
+    Insert(headerList, -1, 3);
+    Insert(headerList, -1, 4);
+    Insert(headerList, -1, 5);
+    Print(headerList);
+    Sort_list(headerList);
+    Print(headerList);
     return 0;
+}
+
+bool Sort_list(HeaderList* L)
+{
+    Node* q = L->head->link;
+    L->head->link = NULL;
+    Node* p, *move;
+    while (q != NULL)
+    {
+        p = q->link;
+        move = L->head;
+        while (move->link != NULL && move->link->element < q->element)
+            move = move->link;
+        q->link = move->link;
+        move->link = q;
+        q = p;
+    }
 }
 
 bool Init(HeaderList* L)
