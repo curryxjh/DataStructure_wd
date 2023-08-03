b66d59191204563f8abeef8a7d340b0225f436b7
curryxjh <2648108535@qq.com>
2023-08-03 23:23:54 +0800

    finish 2_3_7 p17,p18

--- 2_3_7_coding/2_3_7problem18.cpp
+++ 2_3_7_coding/2_3_7problem18.cpp
@@ -7,7 +7,7 @@ using namespace std;
 struct Node
 {
     int element;
-    Node* pre, * ne;
+    Node* link;
 };
 struct HeaderList
 {
@@ -16,42 +16,41 @@ struct HeaderList
 };
 void Print(HeaderList* L);
 bool Init(HeaderList* L);
+void Merge(HeaderList* l1, HeaderList* l2);
 HeaderList* BuildList(int a[], int n);
-bool is_symmetric(HeaderList* L);
-int a[] = {1, 2, 3, 4, 5, 4, 3, 2, 1};
 
+int a[] = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+int b[] = {3, 1, 2, 3, 5};
 int main()
 {
-    HeaderList* L = BuildList(a, sizeof a / 4);
-    Print(L);
-    if (is_symmetric(L))
-        puts("YES");
-    else
-        puts("NO");
+    HeaderList* L1 = BuildList(a, sizeof a / 4);
+    Print(L1);
+    HeaderList* L2 = BuildList(b, sizeof b / 4);
+    Print(L2);
+    Merge(L1, L2);
+    Print(L1);
     return 0;
 }
 
-bool is_symmetric(HeaderList* L)
+void Merge(HeaderList* l1, HeaderList* l2)
 {
-    Node* p = L->head->ne;
-    Node* q = L->head->pre;
-    while (p != q)
-    {
-        if (p->element != q->element)
-            return false;
-        p = p->ne;
-        q = q->pre;
-    }
-    return true;
+    Node* p = l1->head->link;
+    Node* q = l2->head->link;
+    while (p->link != l1->head)
+        p = p->link;
+    while (q->link != l2->head)
+        q = q->link;
+    p->link = l2->head->link;
+    q->link = l1->head;
 }
+
 
 bool Init(HeaderList* L)
 {
     L->head = new Node;
     if (!L->head)
         return false;
-    L->head->ne = L->head;
-    L->head->pre = L->head;
+    L->head->link = L->head;
     L->length = 0;
     return true;
 }
@@ -65,22 +64,20 @@ HeaderList* BuildList(int a[], int n)
     {
         Node* node = new Node;
         node->element = a[i];
-        node->ne = p->ne;
-        node->pre = p;
-        p->ne = node;
-        p = p->ne;
-        L->head->pre = node;
+        node->link = p->link;
+        p->link = node;
+        p = p->link;
     }
     return L;
 }
 
 void Print(HeaderList* L)
 {
-    Node *p = L->head->ne;
+    Node *p = L->head->link;
     while (p != L->head)
     {
         cout << p->element << " ";
-        p = p->ne;
+        p = p->link;
     }
     puts("");
 }
