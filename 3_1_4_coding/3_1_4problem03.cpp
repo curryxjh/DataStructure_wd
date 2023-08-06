06370084c7e5ef960c7700864fac6d29f47ce6b3
curryxjh <2648108535@qq.com>
2023-08-06 23:23:23 +0800

    finsih 3_1_4p03

--- 3_1_4_coding/3_1_4problem03.cpp
+++ 3_1_4_coding/3_1_4problem03.cpp
@@ -1,3 +1,30 @@
-//
-// Created by 86159 on 2023/8/6.
-//
+/*
+ * p03
+ */
+#include <iostream>
+using namespace std;
+string str[] = {"IOIIOIOO", "IOOIOIIO", "IIIOIOIO", "IIIOOIOO"};
+
+int main()
+{
+
+    for (int i = 0; i < 4; ++ i)
+    {
+        int ans = 0;
+        bool flag = true;
+        for (int j = 0; j < str[i].size(); ++ j)
+        {
+            if (str[i][j] == 'I')
+                ++ ans;
+            else if (str[i][j] == 'O')
+                -- ans;
+            if (ans < 0)
+                flag = false;
+        }
+        if (flag && !ans)
+            cout << char('A' + i) << "  合格" << endl;
+        else
+            cout << char('A' + i) << "  不合格" << endl;
+    }
+    return 0;
+}
