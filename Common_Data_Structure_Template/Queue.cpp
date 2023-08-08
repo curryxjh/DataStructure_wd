aed9bc1b3095c27bb6eb677b79cdfaed587e7b3d
curryxjh <2648108535@qq.com>
2023-08-08 22:41:25 +0800

    finsih 3_2_5p02

---
+++ Common_Data_Structure_Template/Queue.cpp
@@ -0,0 +1,53 @@
+/*
+ * 队列
+ */
+#include <iostream>
+#define MaxSize 10
+using namespace std;
+typedef int ElemType;
+struct SqQueue
+{
+    int front, rear;
+    ElemType *data;
+};
+void Init(SqQueue &q);
+bool IsEmpty(SqQueue &q);
+bool EnQueue(SqQueue &q, ElemType x);
+bool DeQueue(SqQueue &q, ElemType &x);
+
+int main()
+{
+    SqQueue q;
+    Init(q);
+
+    return 0;
+}
+
+void Init(SqQueue &q)
+{
+    q.front = q.front = 0;
+    q.data = new ElemType[MaxSize];
+}
+
+bool IsEmpty(SqQueue &q)
+{
+    if (q.front == q.rear)
+        return true;
+    return false;
+}
+
+bool EnQueue(SqQueue &q, ElemType x)
+{
+    if (q.rear == MaxSize)
+        return false;
+    q.data[q.rear ++] = x;
+    return true;
+}
+
+bool DeQueue(SqQueue &q, ElemType &x)
+{
+    if (IsEmpty(q))
+        return false;
+    x = q.data[q.front ++];
+    return true;
+}
